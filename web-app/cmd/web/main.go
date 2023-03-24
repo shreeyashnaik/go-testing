0fcffe29912a20d923d7101b4aecc9487bb64e1a
Shreeyash-Naik <shreeyashnaikofficial@gmail.com>
2023-03-24 06:35:03 +0530

    feat: test post handlers

--- web-app/cmd/web/main.go
+++ web-app/cmd/web/main.go
@@ -1,19 +1,32 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
 	"github.com/alexedwards/scs/v2"
+	"github.com/shreeyashnaik/go-testing/web-app/pkg/db"
 )
 
 type application struct {
 	Session *scs.SessionManager
+	DB      db.PostgresConn
+	DSN     string
 }
 
 func main() {
 	// set up an app config
 	app := application{}
+
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5433 user=postgres password=postgres dbname=users sslmode=disable timezone=IST connect_timeout=5", "Postgres connection")
+	flag.Parse()
+
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.DB = db.PostgresConn{DB: conn}
 
 	// get a session manager
 	app.Session = getSession()
